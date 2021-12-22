3bb37a53afa48d5f2596a5ebf5f7a2c16afb79f0
szollosikrisz99 <96497859+szollosikrisz99@users.noreply.github.com>
2021-12-22 18:40:55 +0100

    Create drill9_4.cpp

---
+++ ch9/4/drill9_4.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+
+
+enum class Month{
+Jan=1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec
+};
+
+
+class Year {
+	static const int min = 1800;
+	static const int max = 2200;
+
+public:
+	class Invalid{};
+	Year(int x) : y{x} {	if(x<min || x>=max) throw Invalid{}; }
+	int year() { return y; }
+	void add_year() { y = ++y; }
+
+private:
+	int y;
+};
+
+
+
+class Date
+{
+	public:
+
+	Date(Year yy, Month mm, int dd);
+	
+	int year();
+	int month();
+	int day();
+
+	void add_day(int n);
+
+private:
+	Year y; 
+	Month m; 
+	int d; 
+};
+
+
+Month operator++(Month& m){
+	m = (m == Month::Dec) ? Month::Jan : Month(int(m)+1);
+	return m;
+}
+
+
+Date::Date(Year yy, Month mm, int dd) : y{yy}, m{mm}, d{dd}
+{	
+}
+	
+	int Date::year() { return y.year(); }
+	int Date::month()  { return int(m); }
+	int Date::day() { return d; }
+
+	void Date::add_day(int n){
+		int newyear_help = 0;
+	if(n>=1){
+		d += n;
+	}
+	if(d>31){
+		d = d-31;
+		if(int(m) = 12){ ++newyear_help; }
+		++m;
+	}
+	if(newyear_help != 0) { y.add_year(); }
+	}
+
+
+
+
+std::ostream& operator<<(std::ostream& os, Date& d) { 
+       
+		os << '(' << d.year() << ',' << d.month() << ',' << d.day() << ')';
+        return os;
+    }
+
+
+void f(){
+	Date today {Year{1978},Month::Jul,25};
+	Date tomorrow = today;
+	std::cout << today;
+	tomorrow.add_day(1);
+	std::cout << tomorrow;
+}
+
+
+
+
+
+int main(){
+
+f();
+
+	return 0;
+}
